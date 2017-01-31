105a07428a87217d8d02618414508b852cc22b5e
Johnie Lee  Fain <jf03436@srcs.loc>
2017-01-31 09:41:44 -0800

    added function tests

--- calculator_tests.js
+++ calculator_tests.js
@@ -14,3 +14,21 @@ QUnit.test( "Add decimal test", function( assert ) {
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
 });
+
+//Test for calculating the factorial of a current input
+QUnit.test( "Calculate the factorial test", function( assert ) {
+    clearCurrent();
+    addDigit('4');
+    factorial();
+    assert.equal(document.getElementById("screen").value, "24", "Passed - Expected 24");
+});
+
+//Test for calculating the trig identity of a current input
+QUnit.test( "Calculate the trig identity test", function( assert ) {
+    clearCurrent();
+    addDigit('3');
+    addDigit('0');
+    switcher();
+    trig("sin");
+    assert.equal(document.getElementById("screen").value, "0.49999999999999994", "Passed - Expected .5");
+});
