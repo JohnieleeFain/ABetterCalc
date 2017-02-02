27c7a9ab60727b5ba36b3ddc01a8f7a178f235dc
Andrea D Martinez <am21325@srcs.loc>
2017-02-02 09:32:28 -0800

    functions

--- calculator_tests.js
+++ calculator_tests.js
@@ -1,16 +1,33 @@
 // Test for inputing digits
-QUnit.test( "Add digits test", function( assert ) {
+QUnit.test("Add digits test", function (assert) {
     clearCurrent();
-    addDigit('1');
     addDigit('2');
-    assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+    assert.equal(document.getElementById("screen").value, "2", "Passed - Expected 2");
 });
-
 // Test adding one, then two decimals
-QUnit.test( "Add decimal test", function( assert ) {
+QUnit.test("Add decimal test", function (assert) {
     clearCurrent();
     addDecimal();
     addDigit('2');
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+});
+QUnit.test("find inverse test", function (assert) {
+    addDigit('4')
+    inverse(current_input);
+    assert.equal(document.getElementById("screen").value, "0.20", "Passed - Expected 0.20");
+    current_input = "0";
+});
+
+QUnit.test("Calculate squareRoot test", function (assert) {
+    addDigit('4');
+    squareRoot(current_input);
+    assert.equal(document.getElementById("screen").value, "2", "Passed - Expected 2");
+    current_input = "0";
+});
+QUnit.test("Solve square test", function (assert) {
+    addDigit('2');
+    square(current_input);
+    assert.equal(document.getElementById("screen").value, "4", "Passed - Expected 4");
+     current_input = "0";
 });
